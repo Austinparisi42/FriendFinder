aefaf8bc9c4cba02a40ab0f4f90b818cfbc12799
Austin Parisi <austinparisi1991@gmail.com>
2017-11-02 18:17:20 -0500

    update

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var bodyParser = require("body-parser");
 
 
 
+
+
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('/app/public'));
 
 
 require("./app/routing/htmlRoutes.js")(app);
